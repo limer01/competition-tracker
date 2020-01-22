a2598184f7fa49c55afddd328c99d491b42aeaf6
limer01 <lalor95@gmail.com>
2020-01-22 00:05:43 +0000

    Started Text connectionn

--- TrackerLib/DataAccess/TextConnector.cs
+++ TrackerLib/DataAccess/TextConnector.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLib.Models;
+using TrackerLib.DataAccess.TextHelpers
 
 namespace TrackerLib.DataAccess
 {
-    public class TextConnector : IDataConnection
+    public class TextConnector : IDataConnection   
     {
+        private const string PrizesFile = "PrizeModels.csv";
         //TODO  Make the create prize method actually save to the textfiles
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
-            return model;
+            //Load Text File
+            //convert to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+            int currentId = prizes.
+
+            //Find ID
+            //Add new record with the new ID
+            //convert prozes to list<String>
+            //save the list>string> to the text file 
+
+      
         }
     }
 }
